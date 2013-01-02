c469719520ccf13ac930ff89304d0c5316c1b5cd
James W Grenning <james@grenning.net>
2013-01-01 22:46:11 -0600

    added styles to make the scoring page less redundant

--- gen/net/grenning/pool_scorekeeper/R.java
+++ gen/net/grenning/pool_scorekeeper/R.java
@@ -27,40 +27,42 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f080016;
+        public static final int Button01=0x7f08001a;
+        public static final int HeaderRow=0x7f080004;
         public static final int ballDownButton=0x7f080008;
+        public static final int consecutiveFoulsRow=0x7f080013;
         public static final int cowboy_pool_button=0x7f080003;
-        public static final int dummy_button=0x7f080017;
+        public static final int currentRackRow=0x7f080007;
+        public static final int dummy_button=0x7f08001b;
         public static final int foulButton=0x7f080010;
         public static final int fullscreen_content=0x7f080000;
         public static final int fullscreen_content_controls=0x7f080001;
-        public static final int menu_settings=0x7f080021;
+        public static final int menu_settings=0x7f080023;
         public static final int missedShotButton=0x7f08000c;
-        public static final int playStraightPoolButton=0x7f08001f;
+        public static final int playStraightPoolButton=0x7f080021;
         public static final int player1BallsThisRack=0x7f080009;
-        public static final int player1ConsecutiveFouls=0x7f080011;
+        public static final int player1ConsecutiveFouls=0x7f080015;
         public static final int player1Name=0x7f080005;
-        public static final int player1PointsToWin=0x7f08001c;
+        public static final int player1PointsToWin=0x7f080011;
         public static final int player1Score=0x7f08000d;
-        public static final int player1TotalFouls=0x7f080014;
+        public static final int player1TotalFouls=0x7f080018;
         public static final int player2BallsThisRack=0x7f08000a;
-        public static final int player2ConsecutiveFouls=0x7f080012;
+        public static final int player2ConsecutiveFouls=0x7f080016;
         public static final int player2Name=0x7f080006;
-        public static final int player2PointsToWin=0x7f08001e;
+        public static final int player2PointsToWin=0x7f080012;
         public static final int player2Score=0x7f08000e;
-        public static final int player2TotalFouls=0x7f080015;
-        public static final int straightPoolPlayer1Row=0x7f08001b;
-        public static final int straightPoolPlayer2Row=0x7f08001d;
-        public static final int straightPoolPlayersTitle=0x7f080019;
-        public static final int straightPoolPointsToWinTitle=0x7f08001a;
-        public static final int straightPoolScore1Row=0x7f080004;
-        public static final int straightPoolScore2Row=0x7f080007;
-        public static final int straightPoolScore3Row=0x7f08000b;
-        public static final int straightPoolScore4Row=0x7f08000f;
-        public static final int straightPoolScore5Row=0x7f080013;
-        public static final int straightPoolStartHeaderRow=0x7f080018;
+        public static final int player2TotalFouls=0x7f080019;
+        public static final int pointsToWinRow=0x7f08000f;
+        public static final int straightPoolPlayer1Row=0x7f08001f;
+        public static final int straightPoolPlayer2Row=0x7f080020;
+        public static final int straightPoolPlayersTitle=0x7f08001d;
+        public static final int straightPoolPointsToWinTitle=0x7f08001e;
+        public static final int straightPoolStartHeaderRow=0x7f08001c;
         public static final int straight_pool_button=0x7f080002;
-        public static final int swapPlayersButton=0x7f080020;
+        public static final int swapPlayersButton=0x7f080022;
+        public static final int totalFoulsRow=0x7f080017;
+        public static final int totalScoreRow=0x7f08000b;
+        public static final int undoButton=0x7f080014;
     }
     public static final class layout {
         public static final int activity_choose_game=0x7f030000;
@@ -81,19 +83,20 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int foul=0x7f050010;
         public static final int game_chooser_screen_content=0x7f050001;
         public static final int game_total=0x7f05000d;
-        public static final int general_pool_rules_button=0x7f05001f;
-        public static final int hello_world=0x7f050021;
-        public static final int lets_play=0x7f05001e;
-        public static final int menu_settings=0x7f050022;
+        public static final int general_pool_rules_button=0x7f050020;
+        public static final int hello_world=0x7f050022;
+        public static final int lets_play=0x7f05001f;
+        public static final int menu_settings=0x7f050023;
         public static final int missed_shot=0x7f050013;
         public static final int missed_shot_total=0x7f050014;
-        public static final int new_game_button=0x7f050020;
+        public static final int new_game_button=0x7f050021;
         public static final int player1=0x7f050017;
         public static final int player2=0x7f050018;
         public static final int player3=0x7f050019;
         public static final int player4=0x7f05001a;
         public static final int players=0x7f050016;
         public static final int playing_straight_pool_screen_content=0x7f050006;
+        public static final int points_needed=0x7f05001e;
         public static final int points_to_win=0x7f05001d;
         public static final int shot_made=0x7f05000b;
         public static final int straight_pool_button=0x7f050007;
@@ -101,7 +104,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int straight_pool_rules_button=0x7f050009;
         public static final int straight_pool_start_content=0x7f050008;
         public static final int swap_players=0x7f05001b;
-        public static final int title_activity_score_straight_pool=0x7f050023;
+        public static final int title_activity_score_straight_pool=0x7f050024;
         public static final int title_activity_straight_pool=0x7f050003;
         public static final int title_activity_straight_pool_start=0x7f050004;
         public static final int total_fouls=0x7f050012;
@@ -136,8 +139,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int AppTheme=0x7f060001;
         public static final int ButtonBar=0x7f060003;
         public static final int ButtonBarButton=0x7f060002;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060004;
+        public static final int FullscreenActionBarStyle=0x7f060007;
+        public static final int FullscreenTheme=0x7f060006;
+        public static final int ScoreTableNumberEntry=0x7f060004;
+        public static final int ScoreTableTextEntry=0x7f060005;
     }
     public static final class styleable {
         /** 
--- src/net/grenning/pool_scorekeeper/ScoreStraightPoolActivity.java
+++ src/net/grenning/pool_scorekeeper/ScoreStraightPoolActivity.java
@@ -15,35 +15,43 @@ public class ScoreStraightPoolActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		setContentView(R.layout.activity_score_straight_pool);
-		
-		setPlayerName("player1Name", R.id.player1Name, R.string.default_player1Name);
-		setPlayerName("player2Name", R.id.player2Name, R.string.default_player2Name);
+
+		setPlayerName("player1Name", R.id.player1Name,
+				R.string.default_player1Name);
+		setPlayerName("player2Name", R.id.player2Name,
+				R.string.default_player2Name);
 		setFieldById(R.id.player1BallsThisRack, 0);
 		setFieldById(R.id.player2BallsThisRack, 0);
+		setFieldById(R.id.player1Score, 0);
+		setFieldById(R.id.player2Score, 0);
+		setFieldById(R.id.player1PointsToWin, getNumberFieldFromIntent("player1PointsToWin"));
+		setFieldById(R.id.player2PointsToWin, getNumberFieldFromIntent("player2PointsToWin"));
 		setFieldById(R.id.player1ConsecutiveFouls, 0);
 		setFieldById(R.id.player2ConsecutiveFouls, 0);
 		setFieldById(R.id.player1TotalFouls, 0);
 		setFieldById(R.id.player2TotalFouls, 0);
-		setFieldById(R.id.player1Score, 0);
-		setFieldById(R.id.player2Score, 0);
-//		setFieldById(R.id.player1PointsToWin, 0);
-//		setFieldById(R.id.player2PointsToWin, 0);
-	}
-	
-	private void setFieldById(int id, int value) {
-		TextView field = (TextView)findViewById(id);
-		field.setText(new Integer(value).toString());
 	}
 
-	private void setPlayerName(String player, int playerTextId, int defaultPlayer) {
+	private void setFieldById(int id, int value) {
+		TextView field = (TextView) findViewById(id);
+		field.setText(Integer.valueOf(value).toString());
+	}
+
+	private void setPlayerName(String player, int playerTextId,
+			int defaultPlayer) {
 		String p1 = getIntent().getStringExtra(player);
-		EditText playerText = (EditText)findViewById(playerTextId);
+		EditText playerText = (EditText) findViewById(playerTextId);
 		if (p1.equals(""))
 			playerText.setText(defaultPlayer);
 		else
 			playerText.setText(p1);
+	}
+
+	private int getNumberFieldFromIntent(String name) {
+		String number = getIntent().getStringExtra(name);
+		return Integer.valueOf(number);
 	}
 
 	@Override
@@ -52,17 +60,18 @@ public class ScoreStraightPoolActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_score_straight_pool, menu);
 		return true;
 	}
-	
-    public void showGeneralPoolRules(View view) {
-    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, 
-    			Uri.parse("http://www.wpa-pool.com/web/the_rules_of_play"));
-    	startActivity(browserIntent);
-    }
-    
-    public void showStraightPoolRules(View view) {
-    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, 
-    			Uri.parse("http://www.wpa-pool.com/web/index.asp?id=119&pagetype=rules"));
-    	startActivity(browserIntent);
-    }
+
+	public void showGeneralPoolRules(View view) {
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+				Uri.parse("http://www.wpa-pool.com/web/the_rules_of_play"));
+		startActivity(browserIntent);
+	}
+
+	public void showStraightPoolRules(View view) {
+		Intent browserIntent = new Intent(
+				Intent.ACTION_VIEW,
+				Uri.parse("http://www.wpa-pool.com/web/index.asp?id=119&pagetype=rules"));
+		startActivity(browserIntent);
+	}
 
 }
