4ed636db758e66bdbab60699d16ce58d8ae5878a
James W Grenning <james@grenning.net>
2013-01-04 00:21:29 -0600

    restructured project to have android and test projects

---
+++ pool-scorekeeper-tests/src/net/grenning/pool_scorekeeper/StraightPoolScorerViewSpy.java
@@ -0,0 +1,37 @@
+package net.grenning.pool_scorekeeper;
+
+public class StraightPoolScorerViewSpy implements StraightPoolScorerView {
+
+	public int player1Score;
+	public int player2Score = 0;
+	public int player1RackScore = 0;
+	public int player1NeededToWin = 0;
+	public int player1ConsecutiveFouls;
+	public int player1TotalFouls;
+
+	@Override
+	public void score(int i) {
+		player1Score = i;
+	}
+
+	@Override
+	public void ballsNeededToWin(int ballsNeededToWin) {
+		this.player1NeededToWin = ballsNeededToWin;
+	}
+
+	@Override
+	public void rackScore(int player1RackScore) {
+		this.player1RackScore = player1RackScore;
+	}
+
+	@Override
+	public void consecutiveFouls(int consecutiveFouls) {
+		player1ConsecutiveFouls = consecutiveFouls;
+	}
+
+	@Override
+	public void fouls(int fouls) {
+		player1TotalFouls = fouls;
+	}
+
+}
