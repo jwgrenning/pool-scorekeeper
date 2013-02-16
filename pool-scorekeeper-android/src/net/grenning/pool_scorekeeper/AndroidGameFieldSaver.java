55a8003e6febc4fb000af33131afe8cfa691f5de
James W Grenning <james@grenning.net>
2013-02-16 13:07:19 -0500

    SharedPreferences problem solved for game, needed o get insance of editor, not just use prefs.edit().putInt

--- pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/AndroidGameFieldSaver.java
+++ pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/AndroidGameFieldSaver.java
@@ -1,6 +1,7 @@
 package net.grenning.pool_scorekeeper;
 
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class AndroidGameFieldSaver implements GameFieldSaver {
@@ -11,24 +12,27 @@ public class AndroidGameFieldSaver implements GameFieldSaver {
 	public AndroidGameFieldSaver(SharedPreferences prefs) {
 		super();
 		this.prefs = prefs;
-		prefs.edit().putInt("junk", -42);
-		prefs.edit().commit();
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt("junk", -42);
+		editor.commit();
 		int value = prefs.getInt("junk", 42);
 		Log.d(this.getClass().getName(), "Create, put -42, commit, getString(" + "junk" + ", " + 42 +") = " + value);
 	}
 
 	@Override
 	public void save(String name, String value) {
+		SharedPreferences.Editor editor = prefs.edit();
 		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
-		prefs.edit().putString(name, value);
-		prefs.edit().commit();
+		editor.putString(name, value);
+		editor.commit();
 	}
 
 	@Override
 	public void save(String name, int value) {
+		SharedPreferences.Editor editor = prefs.edit();
 		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
-		prefs.edit().putInt(name, value);
-		prefs.edit().commit();
+		editor.putInt(name, value);
+		editor.commit();
 	}
 
 	@Override
--- pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/CowboyPoolStartActivity.java
+++ pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/CowboyPoolStartActivity.java
@@ -1,7 +1,10 @@
 package net.grenning.pool_scorekeeper;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 
 public class CowboyPoolStartActivity extends Activity {
@@ -9,6 +12,20 @@ public class CowboyPoolStartActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// SharedPreferences preferences =
+		// PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.putInt("junk", 142);
+		editor.putString("Name", "Harneet");
+		editor.commit();
+		int value = preferences.getInt("junk", 42);
+		Log.d(this.getClass().getName(), "getString("
+				+ "junk" + ", " + 42 + ") = " + value);
+		editor.commit();
+		String svalue = preferences.getString("Name", "default name");
+		Log.d(this.getClass().getName(), "getString(" + "Name ) = " + svalue);
 		setContentView(R.layout.activity_cowboy_pool_start);
 	}
 
