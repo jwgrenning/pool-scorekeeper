5e774f80e468d34635d9442066c6fcf940c17dde
James W Grenning <james@grenning.net>
2013-08-25 11:13:49 -0500

    added index for saving

--- pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/AndroidGameFieldSaver.java
+++ pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/AndroidGameFieldSaver.java
@@ -11,16 +11,20 @@ public class AndroidGameFieldSaver implements NameValueSaver {
 	public AndroidGameFieldSaver(SharedPreferences prefs) {
 		super();
 		this.prefs = prefs;
-//		SharedPreferences.Editor editor = prefs.edit();
-		prefs.edit().putInt("junk", -42).commit();
-//		editor.commit();
-		int value = prefs.getInt("junk", 42);
-		Log.d(this.getClass().getName(), "Create, put -42, commit, getString(" + "junk" + ", " + 42 +") = " + value);
 	}
 
 	@Override
 	public void save(String name, String value) {
 		SharedPreferences.Editor editor = prefs.edit();
+		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
+		editor.putString(name, value);
+		editor.commit();
+	}
+
+	@Override
+	public void save(String name, int index, String value) {
+		SharedPreferences.Editor editor = prefs.edit();
+		name += Integer.valueOf(index).toString();
 		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
 		editor.putString(name, value);
 		editor.commit();
@@ -35,7 +39,24 @@ public class AndroidGameFieldSaver implements NameValueSaver {
 	}
 
 	@Override
+	public void save(String name, int index, int value) {
+		SharedPreferences.Editor editor = prefs.edit();
+		name += Integer.valueOf(index).toString();
+		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
+		editor.putInt(name, value);
+		editor.commit();
+	}
+
+	@Override
 	public void save(String name, boolean value) {
+		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
+		prefs.edit().putBoolean(name, value);
+		prefs.edit().commit();
+	}
+
+	@Override
+	public void save(String name, int index, boolean value) {
+		name += Integer.valueOf(index).toString();
 		Log.d(this.getClass().getName(), "save(" + name + ", " + value +")");
 		prefs.edit().putBoolean(name, value);
 		prefs.edit().commit();
@@ -49,6 +70,14 @@ public class AndroidGameFieldSaver implements NameValueSaver {
 	}
 
 	@Override
+	public String getString(String name, int index, String defaultValue) {
+		name += Integer.valueOf(index).toString();
+		String value = prefs.getString(name, defaultValue);
+		Log.d(this.getClass().getName(), "getString(" + name + ", " + defaultValue +") = " + value);
+		return value;
+	}
+
+	@Override
 	public int getInt(String name, int defaultValue) {
 		int value = prefs.getInt(name, defaultValue);
 		Log.d(this.getClass().getName(), "getString(" + name + ", " + defaultValue +") = " + value);
@@ -56,7 +85,23 @@ public class AndroidGameFieldSaver implements NameValueSaver {
 	}
 
 	@Override
+	public int getInt(String name, int index, int defaultValue) {
+		name += Integer.valueOf(index).toString();
+		int value = prefs.getInt(name, defaultValue);
+		Log.d(this.getClass().getName(), "getString(" + name + ", " + defaultValue +") = " + value);
+		return value;
+	}
+
+	@Override
 	public boolean getBoolean(String name, boolean defaultValue) {
+		boolean value = prefs.getBoolean(name, defaultValue);
+		Log.d(this.getClass().getName(), "getString(" + name + ", " + defaultValue +") = " + value);
+		return value;
+	}
+
+	@Override
+	public boolean getBoolean(String name, int index, boolean defaultValue) {
+		name += Integer.valueOf(index).toString();
 		boolean value = prefs.getBoolean(name, defaultValue);
 		Log.d(this.getClass().getName(), "getString(" + name + ", " + defaultValue +") = " + value);
 		return value;
--- pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/NameValueSaver.java
+++ pool-scorekeeper-android/src/net/grenning/pool_scorekeeper/NameValueSaver.java
@@ -2,9 +2,15 @@ package net.grenning.pool_scorekeeper;
 
 public interface NameValueSaver {
 	void save(String name, String value);
+	void save(String name, int index, String value);
 	void save(String name, int value);
+	void save(String name, int index, int value);
 	void save(String name, boolean value);
+	void save(String name, int index, boolean value);
 	String getString(String name, String defaultValue);
+	String getString(String name, int index, String defaultValue);
 	int getInt(String name, int defaultValue);
+	int getInt(String name, int index, int defaultValue);
 	boolean getBoolean(String name, boolean defaultValue);
+	boolean getBoolean(String name, int index, boolean defaultValue);
 }
